7a1e7fff4e4991c786037205a779a80aca068411
camoalex30 <alex.camouflage.robinson@gmail.com>
2026-02-06 18:15:26 +0000

    ading bday list

---
+++ py/functions and modular code/on-my-own.py
@@ -0,0 +1,20 @@
+from time import sleep
+
+bday = ["memaker arm kit", "pi 5", "Tv for bedroom", "new home for books", "move room around", "3d printer"]
+
+def run ():
+    print("this is what i want for christmas")
+    sleep(1)
+    print(bday[0])
+    sleep(2)
+    print(bday[1])
+    sleep(2)    
+    print(bday[2])
+    sleep(2)
+    print(bday[3])
+    sleep(2)
+    print(bday[4])
+    sleep(2)
+    print(bday[5])
+
+run()
