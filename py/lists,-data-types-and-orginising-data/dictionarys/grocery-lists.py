9e2889ba03a9a834f9a66a22c012afe94093803c
camoalex30 <alex.camouflage.robinson@gmail.com>
2026-02-09 18:04:32 +0000

    adding folder

---
+++ py/lists,-data-types-and-orginising-data/dictionarys/grocery-lists.py
@@ -0,0 +1,15 @@
+grocery_list = []
+#---------menu loop-------------
+while True:
+    print("/n--- grocery lists manager---")
+    print("1.add item")
+    print("2.view list")
+    print("3.removb item")
+    print("4.exit")
+
+#------logic-----
+    choice = input("choose an option(1-4)")
+
+    if choice = = "1":
+        item = input("enter an item to add to list")
+
